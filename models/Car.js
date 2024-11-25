8098e16f9aa964262983ab239079aef86cf2c990
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2024-11-25 23:48:08 +0100

    Add new field for car schema

--- models/Car.js
+++ models/Car.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const carSchema = new mongoose.Schema({
   brand: String,
   model: String,
-  year: String,
+  year: Number,
+  transmission: String,
 });
 
 module.exports = mongoose.model("Car", carSchema);
